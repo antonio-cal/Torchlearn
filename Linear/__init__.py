bee344763b2d635071e9dd3710ac5052484f000c
antonio-cal <antonioicaldeiraicarvalho@gmail.com>
2026-07-23 09:37:27 -0300

    feat: feat: adiciona classe Linear e método fit

---
+++ Linear/__init__.py
@@ -0,0 +1,1 @@
+from .neoronio import *
---
+++ Linear/neoronio.py
@@ -0,0 +1,30 @@
+import torch as to
+import numpy as np
+class Linear:
+    def __init__(self,entradas=1, saidas=1):
+        self.neoronio=to.nn.Linear(entradas, saidas)
+        self.criterio=to.nn.MSELoss()
+        self.optimi=to.optim.SGD(self.neoronio.parameters(), 0.01)
+    def fit(self, X, Y):
+        for i in range(1000):
+            if isinstance(X, np.ndarray):
+                X = to.from_numpy(X).float()
+            else:
+                X = X.float()
+
+            if isinstance(Y, np.ndarray):
+                Y = to.from_numpy(Y).float()
+            else:
+                Y = Y.float()
+            prev=self.neoronio(X)
+            erro=self.criterio(prev, Y)
+            erro.backward()
+            self.optimi.step()
+            self.optimi.zero_grad()
+            
+
+        
+
+
+        
+
